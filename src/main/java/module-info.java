08b3c02ff7953ada9f2c57d059fd29d693a5346e
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2022-02-18 15:38:40 -0500

    run using mvn javafx:run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.alexcomeau {
 
     requires org.fxmisc.richtext;
+    //require all the other modules
     requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+
     exports com.alexcomeau;
 }
