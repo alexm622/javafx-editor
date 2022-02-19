8bdebb7b36208991d545b8d2bff135a7b00dba5e
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2022-02-18 19:49:11 -0500

    fixed warnings:

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,12 @@ module com.alexcomeau {
 
     requires org.fxmisc.richtext;
     //require all the other modules
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
+
+    //add language interface
+    exports com.alexcomeau.lang;
 
     exports com.alexcomeau;
 }
